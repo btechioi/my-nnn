bc54b5543f6f2697f3050d916700cbe9b8e48f5c
Banumath Hettiarachchi <banumath@users.noreply.github.com>
2026-09-09 20:26:49 +0530

    Fix niri cachix public key typo

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       "https://noctalia.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "niri.cachix.org-1:Wv0OmO7PsuocRKzfDo3mulSl7Z6oezYhGhR+3W2964="
+      "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
     ];
   };
